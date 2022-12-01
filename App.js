d52778a30efff811d4d8b3f88047e18ac5528107
Kyle Joseph Timajo <timajokyle24@gmail.com>
2022-12-01 23:54:47 +0800

    feat: paper provider

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { StatusBar, Platform, SafeAreaView } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
 import Main from "./app/main";
 
 function App() {
     return (
-        <SafeAreaView
-            style={{
-                paddingTop:
-                    Platform.OS === "android" ? StatusBar.currentHeight : 0,
-            }}
-            className="flex-1">
-            <Main />
-        </SafeAreaView>
+        <PaperProvider>
+            <SafeAreaView
+                style={{
+                    paddingTop:
+                        Platform.OS === "android" ? StatusBar.currentHeight : 0,
+                }}
+                className="flex-1">
+                <Main />
+            </SafeAreaView>
+        </PaperProvider>
     );
 }
 
