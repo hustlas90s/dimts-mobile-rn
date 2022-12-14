321dfe0b1b24902266a701aa0c01d9837501468f
Kyle Joseph Timajo <timajokyle24@gmail.com>
2022-12-15 01:12:47 +0800

    feat: created redux store

--- app/redux/store.js
+++ app/redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import appSlice from "./features/appSlice";
 
 const store = configureStore({
-    reducer: {},
+    reducer: { appState: appSlice },
 });
 
 export default store;
