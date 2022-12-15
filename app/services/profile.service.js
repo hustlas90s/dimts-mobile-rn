7be792f406e40f80f5b21e6fb707d6905d70f82f
Kyle Joseph Timajo <timajokyle24@gmail.com>
2022-12-16 00:00:37 +0800

    feat: added get and update account api requests

--- app/services/profile.service.js
+++ app/services/profile.service.js
@@ -1,14 +1,10 @@
 import { getData, storeData } from "../helpers/asyncStorage";
-import jwt_decode from "jwt-decode";
 import { api } from "./axios";
 
 export const GetProfile = async () => {
     try {
-        // const token = await getData("access_token");
-        const token =
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc1ODY5NjI1LCJpYXQiOjE2NzEwMzEyMjUsImp0aSI6ImJiYzA4ZGM5NzYxMDRjN2M4NGQ1NDgyMGZlMWRlZTdkIiwidXNlcl9pZCI6Mn0.FFgm7Z7iC3HHQf51pc5bzn3lQjj2V8Zjpxp4c_ACUEE";
-        const id = jwt_decode(token).user_id;
-        console.log(id);
+        const token = await getData("access_token");
+        const id = await getData("user_id");
         const res = await api.get(`/account/${id}`, {
             headers: {
                 Authorization: `Bearer ${token}`,
@@ -16,7 +12,22 @@ export const GetProfile = async () => {
         });
         return res.data;
     } catch (error) {
-        console.log(error);
+        return error.response.data;
+    }
+};
+
+export const UpdateProfile = async (data) => {
+    try {
+        const token = await getData("access_token");
+        const id = await getData("user_id");
+        console.log("User ID: ", id);
+        const res = await api.put(`/account/update/${id}`, data, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        return res.data;
+    } catch (error) {
         return error.response.data;
     }
 };
