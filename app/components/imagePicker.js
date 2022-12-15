dad687c5335f1c1e70f5108e75b1b55684093657
Kyle Joseph Timajo <timajokyle24@gmail.com>
2022-12-15 23:58:55 +0800

    feat: base64 implementation

--- app/components/imagePicker.js
+++ app/components/imagePicker.js
@@ -6,15 +6,14 @@ const ImageUploader = ({ image, setImage, error, setError }) => {
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.All,
+            base64: true,
             allowsEditing: true,
             // aspect: [4, 3],
             quality: 1,
         });
 
-        console.log(result);
-
         if (!result.canceled) {
-            setImage(result.assets[0].uri);
+            setImage(`data:image/jpg;base64,${result.assets[0].base64}`);
             setError(false);
         }
     };
